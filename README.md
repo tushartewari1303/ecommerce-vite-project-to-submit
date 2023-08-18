f46efd522a9611adcbb20f7eecfb3d9820a1da2c
tushartewari1303 <tushartewari1303@gmail.com>
2023-08-18 23:54:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Ecommerce site
+
+#front end
+1.vite react
+2.fit hub created
+
+
+#backend
+
+node 
+express
+axios
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <header> TS ecommerse</header>
+        <main></main>
+        <footer>All Right are reserved</footer>
+       </div>
+    </>
+  )
+}
+
+export default App
